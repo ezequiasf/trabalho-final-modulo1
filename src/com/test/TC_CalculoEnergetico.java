bcb6e4e40013eca56368ea9688f4117cc9283e99
Pablokapp <pablokapp@gmail.com>
2022-02-28 21:10:15 -0300

    Testes CalculoEnergetico e CalculoEnergeticoAtividade

--- src/com/test/TC_CalculoEnergetico.java
+++ src/com/test/TC_CalculoEnergetico.java
@@ -1,4 +1,20 @@
 package com.test;
 
+import com.utils.CalculoEnergetico;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TC_CalculoEnergetico {
+
+    @Test
+    public void testarCalculoEnergetico(){
+        //setup
+        CalculoEnergetico calculoEnergetico = new CalculoEnergetico();
+        Double calorias =
+        calculoEnergetico.calculoGastoEnergetico(1.70,85.0,  "M", 35);
+        Assert.assertTrue(calorias == 1848.22);
+    }
+
 }
+
+
